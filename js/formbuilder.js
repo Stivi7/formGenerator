79d1414823d49b9efd90e4393f64a1d8dcbdffe5
Stivi7 <Stivi7@users.noreply.github.com>
2017-10-03 17:06:49 +0200

    half a way

--- js/formbuilder.js
+++ js/formbuilder.js
@@ -8,17 +8,46 @@ function FormBuilder (input) {
     function build() {
         input = textArea.value; //get the value from text area
         var json = JSON.parse(input); //parse the json input
-        
-        for(var i = 0; i < json.length; i++) {
-            var generateForm = `<input type="${json[i].type}" name="${json[i].name}"></input>`
-            //console.log(json[i].type)
-            outputArea.innerHtml = generateForm;
-        }
+        json.forEach(obj => {
+            var block = createBlock(obj);
+            outputArea.appendChild(block);
+        })        
+
     }
 
 
+    function createElement(jsonCFG) {
+        var el = document.createElement('input');
+        el.setAttribute('class', 'form-control');
+        el.setAttribute("type", jsonCFG.type);
+        el.setAttribute("id", jsonCFG.id);
+        el.setAttribute("name", jsonCFG.name);
+        return el;
+    }
+
+
+     function createLabel(jsonCFG) {
+         var label = document.createElement('label');
+         label.setAttribute('for', jsonCFG.id);
+         label.innerHTML = jsonCFG.label;
+         return label;
+    }
+
+    function createBlock(jsonCFG) {
+        var div = document.createElement('div');
+        var label = createLabel(jsonCFG);
+        var input = createElement(jsonCFG);
+        div.setAttribute('class', 'form-group');
+        div.appendChild(label);
+        div.appendChild(input);
+        return div;
+    }
+
+    
     return {
-        build: build
-        
+        build: build,
+        createElement: createElement,
+        createLabel: createLabel,
+        createBlock: createBlock
     }
 }
