d716596455b018c2897541f1a68d86dc1fff4b60
Stivi7 <Stivi7@users.noreply.github.com>
2017-10-04 21:00:49 +0200

    finished

--- js/formbuilder.js
+++ js/formbuilder.js
@@ -4,93 +4,98 @@
  */
 
 function FormBuilder (input) {
-    
-    function build() {
-        clearForms();
-        input = textArea.value; //get the value from text area
-        
-        try {
-            var json = JSON.parse(input); //parse the json input
-            json.forEach(obj => {
-                var block = createBlock(obj);
-                outputArea.appendChild(block);
-            })        
-    
-        } catch(e) {
-            alert("Invalid Json input: " + e.message);
-        }
-        
+  function build () {
+    clearForms()
+    input = textArea.value // get the value from text area
+
+    try {
+      var json = JSON.parse(input) // parse the json input
+      json.forEach(obj => {
+        var block = createBlock(obj)
+        outputArea.appendChild(block)
+      })
+      debug()
+    } catch(e) {
+      alert('Invalid Json input: ' + e.message)
+    }
+  }
+
+  /**
+   * create an element from json 
+   */
+  function createElement (jsonCFG) {
+    var el = document.createElement('input')
+    el.setAttribute('class', 'form-control')
+    if (jsonCFG.class) el.setAttribute('class', el.getAttribute('class') + ' ' + jsonCFG.class)
+    el.setAttribute('type', jsonCFG.type)
+    el.setAttribute('id', jsonCFG.id)
+    el.setAttribute('name', jsonCFG.name)
+
+    if (jsonCFG.type == 'checkbox' && jsonCFG.checked) {
+      el.setAttribute('checked', 'true')
     }
 
-    /**
-     * create an element from json 
-     */
-    function createElement(jsonCFG) {
-        var el = document.createElement('input');
-        el.setAttribute('class', 'form-control');
-        if (jsonCFG.class) el.setAttribute('class', el.getAttribute('class') + ' ' + jsonCFG.class)
-        el.setAttribute("type", jsonCFG.type);
-        el.setAttribute("id", jsonCFG.id);
-        el.setAttribute("name", jsonCFG.name);
-        
-        if(jsonCFG.type == 'checkbox' && jsonCFG.checked) {
-            el.setAttribute('checked', "true");
-        } 
-              
-        //console.log(jsonCFG.checked);
-    
-        return el;
+    // console.log(jsonCFG.checked)
+
+    return el
+  }
+
+  /** 
+   * create a label  
+   */
+  function createLabel (jsonCFG) {
+    var label = document.createElement('label')
+    label.setAttribute('for', jsonCFG.id)
+    label.innerHTML = jsonCFG.label
+    return label
+  }
+
+  /**
+   * create the block for the input
+   */
+  function createBlock (jsonCFG) {
+    var div = document.createElement('div')
+    var label = createLabel(jsonCFG)
+    var input = createElement(jsonCFG)
+    div.setAttribute('class', 'form-group')
+    div.appendChild(label)
+    div.appendChild(input)
+
+    return div
+  }
+
+  function clearForms () {
+    // console.log('cleared')
+    outputArea.innerHTML = '' // clears the form from previous inputs
+  }
+
+  /**
+   * creates a div with the form html content
+   */
+  function debugArea () {
+    var debugDiv = document.createElement('div')
+    debugDiv.setAttribute('class', 'container debugBg')
+    debugDiv.textContent = outputArea.innerHTML
+    outputArea.appendChild(debugDiv)
+    return debugDiv
+  }
+
+  function debug () {
+    var path = location.href
+    if (path.includes('debug=true')) {
+      debugArea()
     }
-
-    /** 
-     * create a label  
-     */
-     function createLabel(jsonCFG) {
-         var label = document.createElement('label');
-         label.setAttribute('for', jsonCFG.id);
-         label.innerHTML = jsonCFG.label;
-         return label;
-    }
-
-    /**
-     * create the block for the input
-     */
-    function createBlock(jsonCFG) {
-        var div = document.createElement('div');
-        var label = createLabel(jsonCFG);
-        var input = createElement(jsonCFG);
-        div.setAttribute('class', 'form-group');
-        div.appendChild(label);
-        div.appendChild(input);
-    
-        return div;
-    }
+  }
 
 
-    function clearForms() {
-        // console.log('cleared')
-        outputArea.innerHTML = ''; //clears the form from previous inputs
-    }
 
-
-    /**
-     * creates a div with the form html content;
-     */
-    function debugArea() { 
-        var debugDiv = document.createElement('div');
-        debugDiv.textContent = outputArea.innerHTML;
-        outputArea.appendChild(debugDiv);
-        return debugDiv;
-    }
-
-    
-    
-    return {
-        build: build,
-        createElement: createElement,
-        createLabel: createLabel,
-        createBlock: createBlock,
-        clearForms: clearForms,
-        debugArea: debugArea
-    }
+  return {
+    build: build,
+    createElement: createElement,
+    createLabel: createLabel,
+    createBlock: createBlock,
+    clearForms: clearForms,
+    debugArea: debugArea,
+    debug: debug
+  }
 }
